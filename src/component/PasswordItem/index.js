0602195e10616501969f4ecc6e6feefcd503c002
Mayukhdevan <mayukdevansahu@yahoo.com>
2023-02-02 20:39:44 +0000

    No styles added

---
+++ src/component/PasswordItem/index.js
@@ -0,0 +1,47 @@
+import './index.css'
+
+const PasswordItem = props => {
+  const {passwordItem, isHidden, deleteCredential} = props
+  const {id, siteUrl, username, password} = passwordItem
+
+  const onDelete = () => deleteCredential(id)
+
+  const passwordChar = isHidden ? (
+    <img
+      className="masked-password"
+      src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+      alt="stars"
+    />
+  ) : (
+    <p>{password}</p>
+  )
+
+  return (
+    <li className="password-item">
+      <div className="credential-details-container">
+        <div className="site-logo-container">
+          <p>{siteUrl[0].toUpperCase()}</p>
+        </div>
+        <div>
+          <p className="site-url">{siteUrl}</p>
+          <p className="username">{username}</p>
+          <div className="masked-password-box">{passwordChar}</div>
+        </div>
+      </div>
+      <button
+        testid="delete"
+        className="delete-btn"
+        type="button"
+        onClick={onDelete}
+      >
+        <img
+          className="delete-icon"
+          src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+          alt="delete"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default PasswordItem
